68869f5411d5fd0fd68d0e6abfb6faa12b74300b
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-05 19:46:21 +0300

    project structure

--- EcommerceApp_Bulky/Program.cs
+++ EcommerceApp_Bulky/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
