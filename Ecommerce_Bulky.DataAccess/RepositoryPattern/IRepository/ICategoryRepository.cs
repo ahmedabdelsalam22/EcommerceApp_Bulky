bee6a3f00a1f972bf89bec57c9352795ad78a9d8
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-05 20:43:53 +0300

    ICategoryRepository and CategoryRepository

---
+++ Ecommerce_Bulky.DataAccess/RepositoryPattern/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using EcommerceApp_Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Bulky.DataAccess.RepositoryPattern.IRepository
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
---
+++ Ecommerce_Bulky.DataAccess/RepositoryPattern/Repository/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce_Bulky.DataAccess.RepositoryPattern.IRepository;
+using EcommerceApp_Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Bulky.DataAccess.RepositoryPattern.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext Context) : base(Context)
+        {
+            _context = Context;
+        }
+
+        public void Update(Category category)
+        {
+            _context.Update(category);
+        }
+    }
+}
