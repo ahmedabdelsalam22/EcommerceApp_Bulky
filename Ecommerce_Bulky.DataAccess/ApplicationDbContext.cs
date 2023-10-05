e92a8c36e5dc2f5ff77f5ca3f4f7b45d8460e5a2
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-05 20:28:31 +0300

    seed category table to db

--- Ecommerce_Bulky.DataAccess/ApplicationDbContext.cs
+++ Ecommerce_Bulky.DataAccess/ApplicationDbContext.cs
@@ -15,5 +15,15 @@ namespace Ecommerce_Bulky.DataAccess
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category() { Id =1,Name = "Action" , DisplayOrder=1},
+                new Category() { Id =2,Name = "Scifi" , DisplayOrder=2},
+                new Category() { Id =3,Name = "History" , DisplayOrder=3}
+                ) ;
+        }
+
     }
 }
