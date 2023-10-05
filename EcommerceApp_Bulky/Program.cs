13708008cc7c885abc911b50eea48e39ba1880e4
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-05 20:45:39 +0300

    register ICategoryRepository and CategoryRepository DI in program.cs

--- EcommerceApp_Bulky/Program.cs
+++ EcommerceApp_Bulky/Program.cs
@@ -1,4 +1,6 @@
 using Ecommerce_Bulky.DataAccess;
+using Ecommerce_Bulky.DataAccess.RepositoryPattern.IRepository;
+using Ecommerce_Bulky.DataAccess.RepositoryPattern.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,8 @@ builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(option=>option.UseSqlServer(connectionString:connectionString));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
