36ad65e7d0b37990ce7e372cfeba126bcfd53dde
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-05 20:34:57 +0300

    IGenericRepository

---
+++ Ecommerce_Bulky.DataAccess/RepositoryPattern/IRepository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Bulky.DataAccess.RepositoryPattern.IRepository
+{
+    public interface IGenericRepository<T>  where T:class
+    {
+        IEnumerable<T> GetAllAsync(bool tracked = true);
+        T GetByIdAsync(Expression<Func<T,bool>>? fliter=null , bool tracked=true);
+        void CreateAsync(T entity);
+        void Remove(T entity);
+
+    }
+}
