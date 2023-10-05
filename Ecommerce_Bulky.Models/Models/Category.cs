3f34a60707ed9747f7282217650d14173fc28de9
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-06 00:19:14 +0300

    add identity for id col in category

--- Ecommerce_Bulky.Models/Models/Category.cs
+++ Ecommerce_Bulky.Models/Models/Category.cs
@@ -6,6 +6,7 @@ namespace EcommerceApp_Bulky.Models
     public class Category
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
